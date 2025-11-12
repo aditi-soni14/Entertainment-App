0f09858d2aface2f84c6a0a737c91aba156e288e
aditi-soni14 <aditisyashisoni@gmail.com>
2025-11-12 19:16:08 +0530

    make changes in index.js and api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,26 +1,3 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api", // backend URL
-// });
-
-// // Interceptor to attach token with every request if available
-// API.interceptors.request.use((req) => {
-//   // Get the token stored in localStorage
-//   const token = localStorage.getItem("token");
-
-//   // If token exists, add it to the Authorization header
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   // Return the modified request so it can continue
-//   return req;
-// });
-
-// export default API;
-
-
 import axios from "axios";
 
 const API = axios.create({
